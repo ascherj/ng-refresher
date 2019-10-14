d527ac1e1f73510fc1ddaa3e7e5e9e75c97cf03d
jake-ascher <jascher@diversifiedus.com>
2019-10-14 14:42:39 -0400

    Implement routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PersonsComponent } from './persons/persons.component';
 import { PersonInputComponent } from './persons/person-input.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PersonInputComponent } from './persons/person-input.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
