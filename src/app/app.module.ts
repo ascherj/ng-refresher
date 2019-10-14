538abbbc52641aca985ba0da478c3b0eb25f7ca6
jake-ascher <jascher@diversifiedus.com>
2019-10-14 11:37:17 -0400

    Create PersonInputComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PersonsComponent } from './persons/persons.component';
+import { PersonInputComponent } from './persons/person-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonsComponent
+    PersonsComponent,
+    PersonInputComponent
   ],
   imports: [
     BrowserModule
