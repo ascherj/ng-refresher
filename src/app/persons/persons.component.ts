0d34f090e0abdaa4fa3f23b13c5773dcff1eac09
jake-ascher <jascher@diversifiedus.com>
2019-10-14 15:01:09 -0400

    Inject PersonsService into PersonsComponent constructor

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,9 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { PersonsService } from './persons.service';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html'
 })
 export class PersonsComponent {
-  @Input() personList: string[];
+  personList: string[];
+
+  constructor(personsService: PersonsService) {
+    this.personList = personsService.persons;
+  }
 }
